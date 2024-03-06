0bbb6e1d4c66d4aed916e6ab74da98ff9bb68ea3
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-03-06 14:20:53 +0300

    add func sort_operations

--- utils/bank_operations.py
+++ utils/bank_operations.py
@@ -11,3 +11,27 @@ def read_operations():
         operations = json.loads(file)
     return operations
 
+
+def sort_operations():
+    """
+    сортирует список по статусу "EXECUTED"
+    :return:
+    """
+    unsorted_list = read_operations()
+    sorted_list = []
+    for status in unsorted_list:
+        if status == {}:
+            continue
+        elif status['state'] == 'EXECUTED':
+            sorted_list.append(status)
+    return sorted_list
+
+
+
+
+
+
+
+
+
+
